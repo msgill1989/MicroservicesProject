c47e23869eaf30cad6dbcc71ba8718cba0651731
Maninder Gill <maninder.gill77@gmail.com>
2024-02-14 17:36:30 -0700

    changes

--- src/Catalog.API/Controllers/CatalogController.cs
+++ src/Catalog.API/Controllers/CatalogController.cs
@@ -42,7 +42,7 @@ namespace Catalog.API.Controllers
                 return Ok(product);
             }
         }
-        [Route("[action]/category", Name = "GetProductByCategory")]
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
